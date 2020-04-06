db3664184c7707315c216147ab0f48dadd6e9242
Makoa DeVincent <makoadevincent@gmail.com>
2020-04-06 20:00:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,13 +5,19 @@ class Artist
     @name = name
   end
 
+  def add_song(song)
+    song.artist = self
+  end
+
   def songs
+    Song.all.select {|song| song.artist == self}
   end
 
-  def add_song
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
   end
 
-  def add_song_by_name
-  end
-  
+
+
 end
